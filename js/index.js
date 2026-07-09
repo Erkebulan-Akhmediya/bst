e1295e4b45e6cde39731f77659e941af0fe70885
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2026-07-09 12:16:22 +0500

    tested bst

--- js/index.js
+++ js/index.js
@@ -1,23 +1,38 @@
 import BST from "./bst.js"
 
+function cmpArr(arr1, arr2) {
+    if (arr1.length !== arr2.length)
+        return false
+
+    for (let i = 0; i < arr1.length; i++)
+        if (arr1[i] !== arr2[i])
+            return false
+    return true
+}
+
 const bst = new BST()
 
-bst.insert(10)
-bst.insert(8)
-bst.insert(12)
-bst.insert(9)
-bst.insert(11)
+let vals = []
+
+for (let i = 0; i < 10; i++) {
+    const val = Math.floor(Math.random() * 100)
+    vals.push(val)
+    bst.insert(val)
+}
 
 console.log(bst.toArray())
 
-bst.delete(12)
+for (let i = 0; i < 10; i++) {
+    const index = Math.floor(Math.random() * vals.length)
+    const val = vals[index]
+    bst.delete(val)
 
-console.log(bst.toArray())
+    if (index == 0)
+        vals = vals.slice(1)
+    else if (index == vals.length - 1)
+        vals.pop()
+    else
+        vals = [...vals.slice(0, index), ...vals.slice(index+1)]
 
-bst.delete(8)
-
-console.log(bst.toArray())
-
-bst.delete(10)
-
-console.log(bst.toArray())
+    console.log(`deleted ${val}: ${cmpArr(bst.toArray(), vals.toSorted((a, b) => a - b)) ? 'seccess' : `failure (bst: ${bst.toArray()}, vals: ${vals.toSorted((a, b) => a - b)})`}`)
+}
